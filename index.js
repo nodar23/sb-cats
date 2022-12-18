5af01628bcf3e0ea9a4d1802156ea0dfc8373b52
nodar23 <makharadze.nodari@gmail.com>
2022-12-19 01:04:29 +0400

    feat: add css file and change buttons

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const $wr = document.querySelector('[data-wr]');
 
 const getCatHTML = (cat) => {
     return `        
-    <div class="card mb-2" style="width: 18rem;">
-    <img src="${cat.image}" class="card-img-top" alt="${cat.name}">
+    <div class="card mb-2 mx-2" style="width: 18rem;">
+    <img src="${cat.image}" class="card-img-top" alt="${cat.name}" height="250">
     <div class="card-body">
         <h5 class="card-title">${cat.name}</h5>
         <p class="card-text">
         ${cat.description}
         </p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <button type="button" class="btn btn-primary">more</button>
+        <button type="button" class="btn btn-danger">delete</button>
     </div>
 </div>
 `
@@ -26,3 +27,14 @@ fetch('https://cats.petiteweb.dev/api/single/nodar23/show/')
         console.log({ data })
 
     })
+
+// const showModalCat = (cat) => `<div data-card-show>
+//     <img src="${cat.image}" class="card-img-top" alt="${cat.name}">
+  
+//     <div class="card__info"> 
+//       <h3 class="card-title mt-2">${cat.name}</h3>
+//       <p class="card-text text-center p-3">${cat.description}</p>
+//     </div>
+    
+// </div>`;
+
