df288515a4fe043c7f7cfae8844f019f151a1f5e
nodar23 <makharadze.nodari@gmail.com>
2022-12-19 15:38:56 +0400

    feat: add ability button DELETE

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ console.log("work");
 
 const $wr = document.querySelector('[data-wr]');
 
+const ACTIONS = {
+    MORE: 'more',
+    DELETE: 'delete'
+}
+
 const getCatHTML = (cat) => {
     return `        
-    <div class="card mb-2 mx-2" style="width: 18rem;">
+    <div data-cat-id="${cat.id}" class="card mb-4 mx-2" style="width: 18rem;">
     <img src="${cat.image}" class="card-img-top" alt="${cat.name}" height="250">
     <div class="card-body">
         <h5 class="card-title">${cat.name}</h5>
         <p class="card-text">
         ${cat.description}
         </p>
-        <button type="button" class="btn btn-primary">more</button>
-        <button type="button" class="btn btn-danger">delete</button>
+        <button data-action="${ACTIONS.MORE}" type="button" class="btn btn-primary">more</button>
+        <button data-action="${ACTIONS.DELETE}" type="button" class="btn btn-danger">delete</button>
     </div>
 </div>
 `
@@ -21,20 +26,31 @@ const getCatHTML = (cat) => {
 fetch('https://cats.petiteweb.dev/api/single/nodar23/show/')
     .then((res) => res.json())
     .then((data) => {
-
-        $wr.insertAdjacentHTML('afterbegin', data.map(cat => getCatHTML(cat)).join(''))
+        $wr.insertAdjacentHTML(
+            'afterbegin',
+            data.map((cat) => getCatHTML(cat)).join(''),
+        )
 
         console.log({ data })
-
     })
 
-// const showModalCat = (cat) => `<div data-card-show>
-//     <img src="${cat.image}" class="card-img-top" alt="${cat.name}">
-  
-//     <div class="card__info"> 
-//       <h3 class="card-title mt-2">${cat.name}</h3>
-//       <p class="card-text text-center p-3">${cat.description}</p>
-//     </div>
-    
-// </div>`;
+$wr.addEventListener('click', (e) => {
+    if (e.target.dataset.action === ACTIONS.DELETE) {
+        console.log(e.target)
+
+        const $catWr = e.target.closest('[data-cat-id]')
+        const catId = $catWr.dataset.catId
+
+        console.log({ catId })
+
+        fetch(`https://cats.petiteweb.dev/api/single/nodar23/delete/${catId}`, {
+            method: 'DELETE',
+        }).then(res => {
+            if (res.status === 200) {
+                return $catWr.remove()
+            }
+            alert(`Удаление кота с id = ${catId} не удалось`)
+        })
+    }
+})
 
